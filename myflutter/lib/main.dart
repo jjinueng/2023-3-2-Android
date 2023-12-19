6f1da5c89d158d43f34d23a40f7bb5dffa4f1de1
jiyoon <skymap02@naver.com>
2023-12-19 16:34:13 +0900

    no message

--- myflutter/lib/main.dart
+++ myflutter/lib/main.dart
@@ -6,42 +6,70 @@ void main() {
   runApp(MyApp());
 }
 
-class _MyFormState extends State<MyForm> {
-  TextEditingController textEditingController = TextEditingController();
-  String displayedText = 'Hello, Flutter';
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(displayedText, key: Key('displayedText')),
-            TextField(controller: textEditingController, key: Key('textField')),
-            ElevatedButton(
-                child: Text("Change Text!"),
-                onPressed: () => setState(() {
-                  displayedText = textEditingController.text;
-                }),
-                key: Key('changeTextButton')
-            ),
-          ],
-        ));
-  }
-}
-
-class MyForm extends StatefulWidget {
-  @override
-  _MyFormState createState() => _MyFormState();
-}
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Button and TextField Test',
-      home:
-      Scaffold(appBar: AppBar(title: Text('AppBar Title')), body: MyForm()),
+      title: 'ListView Demo',
+      home: Scaffold(
+          appBar: AppBar(title: Text('ListView Suhan')), body: MyBody()),
     );
+  }
+}
+
+class MyBody extends StatelessWidget {
+  Widget _buildList(BuildContext context) {
+    return ListView(children: [
+      _tile(context, 'Roxie Theater', '3117 16th St', Icons.food_bank,
+          const Key('list_item1')),
+      _tile(context, 'United Artists Stonestown Twin', '501 Buckingham Way',
+          Icons.food_bank, const Key('list_item2')),
+      _tile(context, 'AMC Metreon 16', '135 4th St #3000', Icons.food_bank,
+          const Key('list_item3')),
+      _tile(context, 'K\'s Kitchen', '757 Monterey Blvd', Icons.food_bank,
+          const Key('list_item4')),
+      _tile(context, 'La Ciccia', '291 30th St', Icons.food_bank,
+          const Key('list_item5')),
+      _tile(context, 'Roxie Theater', '3117 16th St', Icons.food_bank,
+          const Key('list_item6')),
+      _tile(context, 'United Artists Stonestown Twin', '501 Buckingham Way',
+          Icons.food_bank, const Key('list_item7')),
+      _tile(context, 'AMC Metreon 16', '135 4th St #3000', Icons.food_bank,
+          const Key('list_item8')),
+    ]);
+  }
+
+  Widget _tile(BuildContext context, String title, String subtitle,
+      IconData icon, Key key_) {
+    return ListTile(
+        title: Text(title,
+            style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 20)),
+        subtitle: Text(subtitle),
+        leading: Icon(icon, color: Colors.blue.shade500),
+        onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SubPage(title, subtitle)),
+            ),
+        key: key_);
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return _buildList(context);
+  }
+}
+
+class SubPage extends StatelessWidget {
+  String title;
+  String subtitle;
+
+  SubPage(this.title, this.subtitle);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(title, key: Key('title'))),
+        body: Text(subtitle));
   }
 }
